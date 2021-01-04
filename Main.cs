6c6a0a2e3284bd97e3fa4e2eb6a39b68e23c9997
ultrasonic01 <74746270+ultrasonic01@users.noreply.github.com>
2021-01-04 01:27:38 -0500

    Test version, which is crashing now.

---
+++ Main.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Reflection;
+using UnityModManagerNet;
+using HarmonyLib;
+using I2.Loc;
+
+namespace Solasta_IronLegion_Fix
+{
+    public class Main
+    {
+        [System.Diagnostics.Conditional("DEBUG")]
+        public static void Log(string msg)
+        {
+            if (logger != null) logger.Log(msg);
+        }
+        public static void Error(Exception ex)
+        {
+            if (logger != null) logger.Error(ex.ToString());
+        }
+        public static void Error(string msg)
+        {
+            if (logger != null) logger.Error(msg);
+        }
+
+        public static UnityModManager.ModEntry.ModLogger logger;
+        public static bool enabled;
+
+        static bool Load(UnityModManager.ModEntry modEntry)
+        {
+            try
+            {
+                logger = modEntry.Logger;
+                var harmony = new Harmony(modEntry.Info.Id);
+                harmony.PatchAll(Assembly.GetExecutingAssembly());
+            }
+            catch (Exception ex)
+            {
+                Error(ex);
+                throw;
+            }
+            return true;
+        }
+        [HarmonyPatch(typeof(MainMenuScreen), "RuntimeLoaded")]
+        static class MainMenuScreen_RuntimeLoaded_Patch
+        {
+            static void Postfix()
+            {
+                try
+                {
+                    // Open Iron Legion feat data
+                    // var ironlegionfeat = DatabaseRepository.GetDatabase<FeatDefinition>().GetElement("MightOfTheIronLegion");
+                    var ironlegionarmor = DatabaseRepository.GetDatabase<FeatureDefinitionProficiency>().GetElement("ProficiencyMightOfTheIronLegionArmor");
+                    var HAcategory = DatabaseRepository.GetDatabase<ArmorCategoryDefinition>().GetElement("HeavyArmorCategory");
+                    // var existingproftype = ironlegionarmor.ProficiencyType;
+                    // var existingprof = ironlegionarmor.Proficiencies;
+                    var newproficiency = new FeatureDefinitionProficiency();
+                    AccessTools.Field(ironlegionarmor.GetType(), "proficiencyType").SetValue(newproficiency, 2);
+                    AccessTools.Field(ironlegionarmor.GetType(), "proficiencies").SetValue(newproficiency, HAcategory);
+                }
+                catch (Exception ex)
+                {
+                    Error(ex);
+                    throw;
+                }
+            }
+        }
+    }
+}
+
+
+
